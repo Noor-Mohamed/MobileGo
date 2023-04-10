1cc44a791e18f7ce448118477f457b8eb453a05a
Mohamed Ahmed <47929033+0XMohomiester@users.noreply.github.com>
2023-04-10 20:41:43 +0200

    update signup page, login page and store page

---
+++ MobileGo website/store/script_store.js
@@ -0,0 +1,23 @@
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // for chrome
+    document.documentElement.scrollTop = 0; // for chrome, safari, IE and opera.
+}
+
+window.onload = function() // wait for page to load
+{
+    // Get the button
+    let mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 300px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 650 ) {
+                mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+}
